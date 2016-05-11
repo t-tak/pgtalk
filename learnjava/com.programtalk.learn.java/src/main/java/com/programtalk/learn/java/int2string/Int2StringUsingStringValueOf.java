89510b638c8710576a082c981ad280ebd190d41d
t.tak <t.tak@g.com>
2016-05-11 14:33:25 +0200

    updated

---
+++ learnjava/com.programtalk.learn.java/src/main/java/com/programtalk/learn/java/int2string/Int2StringUsingStringValueOf.java
@@ -0,0 +1,10 @@
+package com.programtalk.learn.java.int2string;
+
+public class Int2StringUsingStringValueOf {
+
+	public static void main(String[] args) {
+		int intValue = 1234;
+		String stringValue = String.valueOf(intValue);
+		System.out.println(stringValue); // prints 1234
+	}
+}
