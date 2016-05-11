7887ead169c173dda96c151ab4387f90b613bccc
t.tak <t.tak@g.com>
2016-05-11 13:58:55 +0200

    added int to string and string to int

---
+++ learnjava/com.programtalk.learn.java/src/main/java/com/programtalk/learn/java/int2string/Int2String.java
@@ -0,0 +1,10 @@
+package com.programtalk.learn.java.int2string;
+
+public class Int2String {
+
+	public static void main(String[] args) {
+		Integer intValue = null;
+		String stringValue = String.valueOf(intValue);
+		System.out.println(stringValue); // prints null
+	}
+}
---
+++ learnjava/com.programtalk.learn.java/src/main/java/com/programtalk/learn/java/int2string/Int2StringIntegerToString.java
@@ -0,0 +1,10 @@
+package com.programtalk.learn.java.int2string;
+
+public class Int2StringIntegerToString {
+
+	public static void main(String[] args) {
+		int intValue = 1234;
+		String stringValue = Integer.toString(intValue);
+		System.out.println(stringValue); // prints 1234
+	}
+}
