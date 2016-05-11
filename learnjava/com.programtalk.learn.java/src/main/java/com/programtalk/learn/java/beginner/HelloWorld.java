7edc69a6edc9415a461f909751161d7a70f0e36b
t.tak <t.tak@g.com>
2016-05-11 13:59:33 +0200

    added a sysout

--- learnjava/com.programtalk.learn.java/src/main/java/com/programtalk/learn/java/beginner/HelloWorld.java
+++ learnjava/com.programtalk.learn.java/src/main/java/com/programtalk/learn/java/beginner/HelloWorld.java
@@ -2,6 +2,6 @@ package com.programtalk.learn.java.beginner;
 
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println();
+	System.out.println("Hello World");
 }
 }
